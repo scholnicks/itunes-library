f464f3e6dc4510919e4fcd8112cda32b0d752419
scholnicks <steve@scholnick.net>
2013-06-09 17:07:24 -0400

    more information for the module

---
+++ itunes/src/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='iTunesLibrary',
+    author='Steve Scholnick',
+    author-email='steve@scholnick.net',
+    Home-page='https://github.com/scholnicks/itunes-library',
+    version='1.0.0',
+    packages=['itunesLibrary',],
+    license='MIT',
+    long_description=open('../../README.md').read(),
+    summary='Representation of an iTunes library'
+)
+
