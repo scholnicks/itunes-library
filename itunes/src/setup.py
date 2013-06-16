fc2c7f4735c27f581e904d05e698bb6041331ca7
scholnicks <steve@scholnick.net>
2013-06-16 12:38:12 -0400

    changes for pypi submission

--- itunes/src/setup.py
+++ itunes/src/setup.py
@@ -3,12 +3,11 @@ from distutils.core import setup
 setup(
     name='iTunesLibrary',
     author='Steve Scholnick',
-    author-email='steve@scholnick.net',
-    Home-page='https://github.com/scholnicks/itunes-library',
-    version='1.0.0',
+    author_email='steve@scholnick.net',
+    version='1.0.1',
     packages=['itunesLibrary',],
     license='MIT',
-    long_description=open('../../README.md').read(),
+    long_description='Represents an iTunes library',
     summary='Representation of an iTunes library'
 )
 
