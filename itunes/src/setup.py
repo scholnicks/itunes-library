e95c64acde80d8b1942f091291ce78cb2ddeef08
scholnicks <steve@scholnick.net>
2013-06-21 15:22:27 -0400

    updated version number

--- itunes/src/setup.py
+++ itunes/src/setup.py
@@ -4,7 +4,7 @@ setup(
     name='iTunesLibrary',
     author='Steve Scholnick',
     author_email='steve@scholnick.net',
-    version='1.0.2',
+    version='1.0.3',
     packages=['itunesLibrary',],
     license='MIT',
     long_description='Represents an iTunes library',
