675bd9733c3459567b106a384cdca5fc3cd71f87
Steve Scholnick <scholnicks@gmail.com>
2026-04-04 09:04:41 -0400

    fixed classifiers

--- src/itunesLibrary/__init__.py
+++ src/itunesLibrary/__init__.py
@@ -1,15 +1,1 @@
 # vi: set syntax=python ts=4 sw=4 sts=4 et ff=unix ai si :
-
-"""
-
-(c) Steve Scholnick <scholnicks@gmail.com>
-MIT License
-
-    https://github.com/scholnicks/itunes-library
-"""
-
-name = "itunes-library"
-__author__ = "Steven Scholnick <scholnicks@gmail.com>"
-__version__ = "1.2.1"
-__copyright__ = "Copyright (c) 2013- Steven Scholnick"
-__license__ = "MIT"
